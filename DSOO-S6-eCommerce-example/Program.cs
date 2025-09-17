c8d5e937990c991e1ba1daab42c8740ecc0b834f
Lucas Luccaroni <lucas_luccaroni@hotmail.com.ar>
2025-09-17 14:09:49 -0300

    Add project files.

---
+++ DSOO-S6-eCommerce-example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DSOO_S6_eCommerce_example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
